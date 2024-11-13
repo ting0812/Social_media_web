e21a977afa06869946356534800f696ffddd67a6
ting0812 <61533369+ting0812@users.noreply.github.com>
2024-11-13 23:52:07 +0800

    v1

---
+++ test_web/src/view/FrontLayout.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+      <div class="row pt-3">
+        <div class="col"></div>
+        <div class="col-3 my-auto">
+          <form id="form">
+            <h1 class="mb-4 text-center">建立新的帳號</h1>
+            <input
+              type="text"
+              placeholder="請輸入帳號"
+              class="mb-3 px-3 pe-4 py-2"
+            />
+            <input
+              type="password"
+              placeholder="請輸入密碼"
+              class="mb-4 px-3 pe-4 py-2"
+            />
+            <button class="btn btn-lg btn-primary w-100" type="submit">
+              註冊
+            </button>
+            <button class="btn btn-lg btn-primary w-100" type="submit">
+              登入
+            </button>
+          </form>
+        </div>
+        <div class="col"></div>
+      </div>
+    </div>
+  </template>
